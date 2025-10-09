aedcacf82dff45ab40946082c4bc52e9dcdc0112
Gurpreet <ajbagdi6@gmail.com>
2025-10-09 17:57:50 +0530

    with backend and i am working

--- src/Components/Searchcop.jsx
+++ src/Components/Searchcop.jsx
@@ -1,31 +1,49 @@
 import { useState } from "react";
 import Tasklist from "./Tasklist";
 
-export default function Searchcop({addBtn,inpValue,setfun}) {
- 
+export default function Searchcop({
+  addBtn,
+  inpValue,
+  setfun,
+  setDes,
+  desValue,
+})
 
 
+
+{
   return (
-    <div >
-      <h1 className="text-2xl font-bold text-center mb-5 tracking-wider">TODO LIST</h1>
+    <div>
+      <h1 className="text-2xl font-bold text-center mb-5 tracking-wider">
+        TODO LIST
+      </h1>
       <div>
         <div>
           <input
             type="text"
-            placeholder="Enter Items..."
+            placeholder="Enter title..."
             onChange={(event) => setfun(event.target.value)}
-            className="border-1 border-gray-400 w-full text-lg p-1 rounded"
+            className="border-1 border-gray-400 w-30 text-lg p-1 rounded"
             value={inpValue}
           />
+          <input
+            type="text"
+            placeholder="Description"
+            onChange={(event) => setDes(event.target.value)}
+            className="border-1 ml-2 border-gray-400 w-30 text-lg p-1 rounded"
+            value={desValue}
+          />
           <br />
-          <button onClick={addBtn} className="border mt-1 px-2 rounded bg-black text-white">
+          <button
+            onClick={addBtn}
+            className="border mt-1 px-2 rounded bg-black text-white"
+          >
             Add
           </button>
-
+{console.log(desValue)}
           <br />
         </div>
         {/* all tasks props */}
-        
       </div>
     </div>
   );
--- src/Components/Tasklist.jsx
+++ src/Components/Tasklist.jsx
@@ -1,16 +1,56 @@
 import { useState } from "react";
 import Searchcop from "./Searchcop";
+import { useEffect } from "react";
 
 export default function Tasklist() {
   let [inpValue, setInpValue] = useState("");
+  let [description, setDescription] = useState("");
   let [taskObj, settaskObj] = useState([]);
   // let [complated,setcomplated] = useState(false);
-  let addedTask = () => {
-    if (inpValue.trim() !== "" ) {
-      settaskObj([...taskObj, { task: inpValue, comp: false }]);
+  let addedTask = async () => {
+    if (inpValue.trim() !== "") {
+      settaskObj([
+        ...taskObj,
+        { task: inpValue, des: description, comp: false },
+      ]);
       setInpValue("");
+      setDescription("");
+    }
+
+    try {
+      await fetch(`http://localhost:5000/add`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(taskObj),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to submit");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  // useEffect(async() =>
+  // {
+  // try {
+  //       await fetch(`http://localhost:5000/add`, {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify(taskObj),
+  //       });
+
+  //       if (!res.ok) {
+  //         throw new Error("Failed to submit");
+  //       }
+
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // ,[addedTask])
+
   // *deleteTask function
   let deleteTask = (index) => {
     let delT = taskObj.filter((_, i) => i !== index);
@@ -26,49 +66,70 @@ export default function Tasklist() {
   };
   console.log(taskObj);
   return (
-    <div className="flex justify-center items-center h-[100vh]">
+    <div className="flex justify-start items-center h-[100vh]">
+      <div className="border-red-500 border-2 w-1/3 p-10 ">
+        {/* search bar and btn */}
+        <Searchcop
+          addBtn={addedTask}
+          inpValue={inpValue}
+          setfun={setInpValue}
+          setDes={setDescription}
+          desValue={description}
+        />
 
-    <div className="border-red-500 border-2 w-1/3 p-10 ">
-      {/* search bar and btn */}
-      <Searchcop addBtn={addedTask} inpValue={inpValue} setfun={setInpValue} />
+        {/* tasks list */}
+        {taskObj.map((items, i) => {
+          return (
+            <div key={i} className="">
+              {items.comp ? (
+                <div>
+                  <p className="bg-red-500 text-white px-1 border-gray-700 border-1 mt-1 relative p-1 w-full">
+                    {items.task}
+                    <button
+                      className="border-1 absolute right-22 text-gray-400 bg-white rounded px-1 text-sm pt-1"
+                      onClick={() => deleteTask(i)}
+                    >
+                      Delete
+                    </button>
 
-      {/* tasks list */}
-      {taskObj.map((items, i) => {
-        return (
-          <div key={i} className="">
-           {items.comp ?  <p className="bg-red-500 text-white px-1 border-gray-700 border-1 mt-1 relative p-1 w-full">
-              {items.task}
-              <button
-                className="border-1 absolute right-22 text-gray-400 bg-white rounded px-1 text-sm pt-1"
-                onClick={() => deleteTask(i)}
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => complatedTaskFunc(i)}
-                className="border-1 absolute right-0  text-blue-500 font-bold bg-white rounded px-1  text-sm pt-1 w-20"
-              >
-                Complated
-              </button>
-            </p> :  <p className="bg-gray-300 text-gray-700 px-1 border-gray-700 border-1 mt-1 relative p-1">
-              {items.task}
-              <button
-                className="border-1 absolute right-27  bg-white rounded px-1 text-sm pt-1"
-                onClick={() => deleteTask(i)}
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => complatedTaskFunc(i)}
-                className="border-1 absolute right-0  bg-white rounded px-1  text-sm pt-1  w-25"
-              >
-                Do complate
-              </button>
-            </p>}
-          </div>
-        );
-      })}
-    </div>
+                    <button
+                      onClick={() => complatedTaskFunc(i)}
+                      className="border-1 absolute right-0  text-blue-500 font-bold bg-white rounded px-1  text-sm pt-1 w-20"
+                    >
+                      Complated
+                    </button>
+                  </p>{" "}
+                </div>
+              ) : (
+                <div>
+                  <p className="bg-gray-300 text-gray-700 px-1 border-gray-700 border-1 mt-1 relative p-1">
+                    {items.task}
+                    <button
+                      className="border-1 absolute right-27  bg-white rounded px-1 text-sm pt-1"
+                      onClick={() => deleteTask(i)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => complatedTaskFunc(i)}
+                      className="border-1 absolute right-0  bg-white rounded px-1  text-sm pt-1  w-25"
+                    >
+                      Do complate
+                    </button>
+                  </p>
+                  <p className="bg-gray-300 text-yellow-500 px-1 border-gray-700 border-1 mt-1 relative p-1">
+                    {" "}
+                    Description :{" "}
+                    <span className="text-blue-600">{items.des}</span>
+                  </p>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
+
+// module.exports = taskObj;
